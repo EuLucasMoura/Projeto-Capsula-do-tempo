7df46c9320bd9bee770f24351551234114c1b141
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2023-08-27 14:19:17 -0300

    Adicionando eslint ao servidor

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
-app.listen({
-port: 3333,
-}).then(() => {
-    console.log("🚀 O servidor está rodando em http://localhost:3333 🚀")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 O servidor está rodando em http://localhost:3333 🚀')
+  })
